d90826be93ec88923009846827ea62b0e36c5c60
sm-123456 <satwikmathi13@gmail.com>
2026-09-08 17:54:10 +0100

    Add files via upload

--- L5/main.py
+++ L5/main.py
@@ -1,27 +1,50 @@
 import pgzrun 
+from random import randint 
 
 WIDTH=1000
 HEIGHT=650
+score=0
 
-Alien=Actor("alien")
+Alien=Actor("flower")
 Alien.pos=(500,325)
-Astronaut=Actor("screenshot 2026-08-18 175945")
+Astronaut=Actor("bee")
 Astronaut.pos=(120,150)
 
 
 def draw():
-    screen.fill("darkgray")
+    global gameover
+    screen.fill("white")
     Alien.draw()
     Astronaut.draw()
+    screen.draw.text(f"Score: {score}", (10, 10), fontsize=30, color="black")
+    if gameover==True:
+        screen.fill("grey")
+        screen.draw.text(f"Game Over\nFinal score: {score}", (450, 300), fontsize=30, color="black")   
+
+gameover=False
+
+def end():
+    global gameover
+    gameover=True
 
 def update(): 
-    if keyboard.left: 
+    global score
+    if keyboard.left or keyboard.a: 
         Astronaut.x=Astronaut.x-10
-    if keyboard.right: 
+    if keyboard.right or keyboard.d: 
         Astronaut.x=Astronaut.x+10
-
-
-
-
-
+    if keyboard.up or keyboard.w: 
+        Astronaut.y=Astronaut.y-10
+    if keyboard.down or keyboard.s: 
+        Astronaut.y=Astronaut.y+10
+    if Alien.colliderect(Astronaut):
+        score+=1
+        while True:
+            Num1=randint(100,900)
+            Num2=randint(100,550)
+            Alien.pos=(Num1,Num2)
+            if not Alien.colliderect(Astronaut):
+                break
+clock.schedule(end,10)
+        
 pgzrun.go()
