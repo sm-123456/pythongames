3052762a00f86773602f84c5eb92fa51a6e82b52
sm-123456 <satwikmathi13@gmail.com>
2026-09-15 17:58:56 +0100

    Add files via upload

---
+++ L6/main.py
@@ -0,0 +1,20 @@
+letters=["a","b","c","d","e","f"]
+
+n=0
+
+#while n<6:
+#    print(letters[n])
+#    n+=1  
+
+#for i in range(0,5,1):
+#    print(letters[i])
+
+#for i in letters:
+#    print(i)
+
+letters2=[["a","c"],["d","f"],["g","i"]]
+
+print(letters2[1][1])
+
+for i in range(0,3,1):
+    print(letters2[i])
