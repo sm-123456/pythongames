bf5da9da2599f92fe77afdf241460056ecf9d586
sm-123456 <satwikmathi13@gmail.com>
2026-09-22 17:57:30 +0100

    Add files via upload

---
+++ L6/Lists.py
@@ -0,0 +1,34 @@
+letters=["a","b","c","d","e","f"]
+
+n=0
+m=0
+
+#while n<6:
+#    print(letters[n])
+#    n+=1  
+
+#for i in range(0,5,1):
+#    print(letters[i])
+
+#for i in letters:
+#    print(i)
+
+letters2=[["a","c"],["d","f"],["g","i"]]
+
+for i in letters2:
+    for q in i:
+        print(q)
+
+#print(letters2[1][1])
+
+#for i in range(0,3,1):
+ #   ac=letters2[0]
+    
+    # print(letters2[i])
+
+# while n<3 and m<3:
+#     print(letters2[n][m])
+#     m+=1
+#     print(letters2[n][m])
+#     n+=1
+#     m+=1
